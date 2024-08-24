848b3f4271f0bae61faddf77db6bcab9777f4c13
Max Mason <maxdoylemason@gmail.com>
2024-08-23 19:50:05 -0600

    add colors and icons

---
+++ src/styles/colors.js
@@ -0,0 +1,79 @@
+export const colors = {
+  white: "#ffffff",
+  gray01: "#eaecef",
+  gray02: "#dfe2e6",
+  gray03: "#cfd4d9",
+  gray04: "#aeb5bc",
+  gray05: "#6e757c",
+  gray06: "#4a5056",
+  gray07: "#353a3f",
+  blue00: "#557c93",
+  blue01: "#183f69",
+  blue02: "#15395e",
+  blue03: "#123253",
+  blue04: "#0f2d4c",
+  blue05: "#0d2843",
+  blue06: "#0b243c",
+  blue07: "#092035",
+  blue08: "#06192b",
+  blue085: "#08203e",
+  blue09: "#041522",
+  blue10: "#03111b",
+  black00: "#0A0C10",
+  black: "#000000",
+};
+
+export const palette1 = {
+  primary: "#000000",
+  secondary: "#323232",
+  tertiary: "#FF1E56",
+  quaternary: "#FFAC41",
+  white: "#FFFFFF",
+};
+
+export const gradients = {
+  darkBlue: `linear-gradient(180deg, #507988 0%, #15395e 100%)`,
+  dark: `linear-gradient(180deg, #353a3f 0%, #000 100%)`,
+  light: `linear-gradient(180deg, #eaecef 0%, #aeb5bc 100%)`,
+
+  grayGray: `linear-gradient(180deg, #303136 0%, #222325 100%)`,
+  blueBlue: `linear-gradient(180deg, #243974 0%, #172448 100%)`,
+  solidBlueBlue: `linear-gradient(180deg, #405EB2 0%, #2D4484 100%)`,
+};
+
+export const colorPalette = {
+  background: {
+    blue1: "#0C111C",
+    blue2: "#111725",
+    black1: "#111113",
+    black2: "#19191B",
+  },
+  components: {
+    blue1: "#172448",
+    blue2: "#1D2E61",
+    blue3: "#243974",
+    gray1: "#222325",
+    gray2: "#292A2E",
+    gray3: "#303136",
+  },
+  borders: {
+    blue1: "#2D4484",
+    blue2: "#375098",
+    blue3: "#405EB2",
+    gray1: "#393A40",
+    gray2: "#46484F",
+    gray3: "#5F606A",
+  },
+  solids: {
+    blue1: "#3D63DD",
+    blue2: "#3F5CB0",
+    gray1: "#6C6E79",
+    gray2: "#797B86",
+  },
+  text: {
+    blue1: "#93B4FF",
+    blue2: "#D5E2FF",
+    gray1: "#B2B3BD",
+    gray2: "#EEEEF0",
+  },
+};
