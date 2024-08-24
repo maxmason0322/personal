4dfa32b5c09139ea6f559f7a1f1998b932156167
Max Mason <maxdoylemason@gmail.com>
2024-08-23 19:48:41 -0600

    added Nunito font and styles

---
+++ src/styles/text.js
@@ -0,0 +1,29 @@
+import { css } from "styled-components";
+
+const textStyles = {
+  body: css`
+    font-family: Nunito, sans-serif;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 120%;
+    letter-spacing: -0.2px;
+  `,
+  body2: css`
+    font-family: Nunito, sans-serif;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 120%;
+    letter-spacing: -0.2px;
+  `,
+  message: css`
+    font-family: Nunito, sans-serif;
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 200;
+    line-height: 90%;
+  `,
+};
+
+export default textStyles;
